211e73907cdd36eff6ecec17795ab79f4dd1d872
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-02-26 17:05:06 +0200

    .

---
+++ script.js
@@ -0,0 +1,29 @@
+const q = Number(prompt("Введіть перше число для додавання:"));
+const c = Number(prompt("Введіть друге число для додавання:"));
+
+alert(q + " + " + c + " = " + (q + c));
+
+const e = Number(prompt("Введіть перше число для віднімання:"));
+const r = Number(prompt("Введіть друге число для віднімання:"));
+
+alert(e + " - " + r + " = " + (e - r));
+
+const t = Number(prompt("Введіть перше число для множення:"));
+const u = Number(prompt("Введіть друге число для множення:"));
+
+alert(t + " * " + u + " = " + (t * u));
+
+const i = Number(prompt("Введіть перше число для ділення:"));
+const o = Number(prompt("Введіть друге число для ділення:"));
+
+alert(i + " / " + o + " = " + (i / o));
+
+console.log(10>7);
+console.log(4*7);
+
+Math.round(7.8)
+console.log(Math.round(7.8));
+
+const x = 50;
+const y = 30;
+console.log(50-30);
